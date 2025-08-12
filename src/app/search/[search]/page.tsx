cdc689d182090170c04606c89b372c1b69915d9b
bharney <bharney0@gmail.com>
2025-08-11 22:54:37 -0500

    Refactor Page component to await params for improved async handling of search query

--- src/app/search/[search]/page.tsx
+++ src/app/search/[search]/page.tsx
@@ -2,8 +2,12 @@ import * as React from "react";
 import { searchArticles } from "../../services/article-service";
 import SearchResults from "../../components/search-results";
 
-export default async function Page({ params }: { params: { search: string } }) {
-  const { search } = params;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ search: string }>;
+}) {
+  const { search } = await params;
   const articles = await searchArticles(search);
 
   return (
